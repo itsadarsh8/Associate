5872006850151bdd1149eadd3487a4e852ad915c
Adarsh <itsadarsh8@gmail.com>
2020-06-12 17:04:34 +0530

    Added callback

--- app/src/main/java/com/example/associate/ArchitectureComponents/WordRoomDatabase.java
+++ app/src/main/java/com/example/associate/ArchitectureComponents/WordRoomDatabase.java
@@ -1,10 +1,13 @@
 package com.example.associate.ArchitectureComponents;
 
 import android.content.Context;
+import android.os.AsyncTask;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 import com.example.associate.Object.Word;
 
@@ -19,10 +22,44 @@ public abstract class WordRoomDatabase extends RoomDatabase {
             INSTANCE= Room.databaseBuilder(context.getApplicationContext(),
                     WordRoomDatabase.class,"word_database")
                     .fallbackToDestructiveMigration()
+                    .addCallback(sRoomDatabaseCallback)
                     .build();
         }
         return  INSTANCE;
     }
 
+    private static RoomDatabase.Callback sRoomDatabaseCallback =
+            new RoomDatabase.Callback(){
+
+                @Override
+                public void onOpen (@NonNull SupportSQLiteDatabase db){
+                    super.onOpen(db);
+                    new PopulateDbAsync(INSTANCE).execute();
+                }
+            };
+
+    private static class PopulateDbAsync extends AsyncTask<Void, Void, Void> {
+
+        private final WordDao mDao;
+        String[] words = {"dolphin", "crocodile", "cobra"};
+
+        PopulateDbAsync(WordRoomDatabase db) {
+            mDao = db.wordDao();
+        }
+
+        @Override
+        protected Void doInBackground(final Void... params) {
+            // Start the app with a clean database every time.
+            // Not needed if you only populate the database
+            // when it is first created
+            mDao.deleteAll();
+
+            for (int i = 0; i <= words.length - 1; i++) {
+                Word word = new Word(words[i]);
+                mDao.insert(word);
+            }
+            return null;
+        }
+    }
 
 }
